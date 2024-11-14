b94c8635f55e440f9bcbbc9ba8dadf1f752507a9
Sara-Talebi <stalebi@g.syr.edu>
2024-11-14 08:05:13 -0500

    Update emcee_mcmc.py
    setting up the emcee and ploting the histogram of the posterior samples and overlay the theoretical distribution of 3s, and plot the trace of walker

--- RadialDist/emcee_mcmc.py
+++ RadialDist/emcee_mcmc.py
@@ -2,8 +2,6 @@ import numpy as np
 import emcee
 import matplotlib.pyplot as plt
 
-a0 = 5.29e-11
-
 ##-----------------------------------------------------------##
 def radial_func(r):
     normalization = 1 / (81 * np.sqrt(3 * a0**3))
@@ -19,3 +17,41 @@ def log_prob(r):
         return -np.inf
     return np.log(radial_prob_func(r[0]) + 1e-100)
 ##-----------------------------------------------------------##
+def main():
+    # Set up MCMC parameters
+    a0 = 5.29e-11
+    ndim = 1
+    nwalkers = 50
+    nsteps = 5000
+    
+    initial_positions = np.array([5 * a0 + 1e-10 * np.random.randn(ndim) for i in range(nwalkers)])
+    
+    # Run emcee sampler
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob)
+    sampler.run_mcmc(initial_positions, nsteps, progress=True)
+    samples = sampler.get_chain(discard=int(nsteps * 0.2), flat=True)
+    
+    # Plot the trace of the first walker
+    plt.figure()
+    plt.title("Trace of the first walker")
+    plt.plot(sampler.get_chain()[:, 0, 0])
+    plt.xlabel("Iteration")
+    plt.ylabel("Radial distance r (m)")
+    plt.show()
+    
+    # Plot histogram of the posterior samples
+    plt.figure()
+    plt.title("Posterior samples of radial distance")
+    plt.hist(samples, bins=100, density=True, alpha=0.7, color='b')
+    plt.xlabel("Radial distance r (m)")
+    plt.ylabel("Probability density")
+    
+    # Overlay theoretical distribution
+    r_values = np.linspace(0, 20 * a0, 1000)
+    theoretical_distribution = [radial_prob_func(r) for r in r_values]
+    plt.plot(r_values, theoretical_distribution, 'r-', label="Theoretical 3s radial distribution")
+    plt.legend()
+    plt.show()
+
+##-----------------------------------------------------------##
+main()
